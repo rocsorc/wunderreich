7a9883f958fe8db0279ee2c2e8678520e1a7c88c
Frank Bauer <frank.bauer@fau.de>
2024-07-30 23:13:01 +0200

    [Change] Added back support for REI

--- src/main/java/de/ambertation/wunderreich/integration/rei/ServerPlugin.java
+++ src/main/java/de/ambertation/wunderreich/integration/rei/ServerPlugin.java
@@ -1,42 +1,30 @@
 package de.ambertation.wunderreich.integration.rei;
 
-//import de.ambertation.wunderreich.Wunderreich;
-//import de.ambertation.wunderreich.items.TrainedVillagerWhisperer;
-//import de.ambertation.wunderreich.recipes.ImprinterRecipe;
-//import de.ambertation.wunderreich.registries.WunderreichItems;
-//
-//import net.minecraft.nbt.CompoundTag;
-//import net.minecraft.nbt.Tag;
-//import net.minecraft.world.item.ItemStack;
-//
-//import me.shedaniel.rei.api.common.category.CategoryIdentifier;
-//import me.shedaniel.rei.api.common.entry.comparison.EntryComparator;
-//import me.shedaniel.rei.api.common.entry.comparison.ItemComparatorRegistry;
-//import me.shedaniel.rei.api.common.plugins.REIServerPlugin;
-//
-//import java.util.function.Function;
-//
-//public class ServerPlugin implements REIServerPlugin {
-//    public static CategoryIdentifier<ImprinterDisplay> IMPRINTER = CategoryIdentifier.of(
-//            Wunderreich.MOD_ID,
-//            ImprinterRecipe.Type.ID.getPath()
-//    );
-//
-//    @Override
-//    public void registerItemComparators(ItemComparatorRegistry registry) {
-//        EntryComparator<Tag> nbtHasher = EntryComparator.nbt();
-//        Function<ItemStack, CompoundTag> enchantmentTag = stack -> {
-//            CompoundTag tag = stack.getTag();
-//            if (tag == null) return null;
-//            if (tag.contains(TrainedVillagerWhisperer.TAG_NAME, Tag.TAG_COMPOUND)) {
-//                return tag.getCompound(TrainedVillagerWhisperer.TAG_NAME);
-//            }
-//            return new CompoundTag();
-//        };
-//
-//        registry.register(
-//                (context, stack) -> nbtHasher.hash(context, enchantmentTag.apply(stack)),
-//                WunderreichItems.WHISPERER
-//        );
-//    }
-//}
+import de.ambertation.wunderreich.Wunderreich;
+import de.ambertation.wunderreich.recipes.ImprinterRecipe;
+import de.ambertation.wunderreich.registries.WunderreichItems;
+
+import net.minecraft.core.component.DataComponentMap;
+
+import static de.ambertation.wunderreich.registries.WunderreichDataComponents.WHISPERER;
+import me.shedaniel.rei.api.common.category.CategoryIdentifier;
+import me.shedaniel.rei.api.common.entry.comparison.EntryComparator;
+import me.shedaniel.rei.api.common.entry.comparison.ItemComparatorRegistry;
+import me.shedaniel.rei.api.common.plugins.REIServerPlugin;
+
+public class ServerPlugin implements REIServerPlugin {
+    public static CategoryIdentifier<ImprinterDisplay> IMPRINTER = CategoryIdentifier.of(
+            Wunderreich.MOD_ID,
+            ImprinterRecipe.Type.ID.getPath()
+    );
+
+    @Override
+    public void registerItemComparators(ItemComparatorRegistry registry) {
+        final EntryComparator<DataComponentMap> nbtHasher = EntryComparator.component(WHISPERER);
+
+        registry.register(
+                (context, stack) -> nbtHasher.hash(context, stack.getComponents()),
+                WunderreichItems.WHISPERER
+        );
+    }
+}
